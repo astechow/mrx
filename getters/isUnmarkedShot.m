35618ba5f3cd96b3e9bd05fd2611be8e0c4dac88
Adrian von Stechow <adrian@stechow.org>
2016-01-07 17:49:52 +0100

    speed up through option of not loading shotdb on every run

--- getters/isUnmarkedShot.m
+++ getters/isUnmarkedShot.m
@@ -1,13 +1,16 @@
-function index = isUnmarkedShot(shot)
-% index = isUnmarkedShot(shot)
+function index = isUnmarkedShot(shot,m)
+% index = isUnmarkedShot(shot,dbmat)
 %
 % returns the index in shotdb of a shot number if "marked" field is false,
 % 0 if true or if shot is unavailable
+% faster if shotdb matfile object is supplied as optional argument dbmat
 %
 % Jan. 2016, Adrian von Stechow
 
-c = initMRX;
-m = matfile(c.dbPath);
+if nargin == 1
+    c = initMRX;
+    m = matfile(c.dbPath);
+end
 
 index  = find(m.shot==shot);
 
